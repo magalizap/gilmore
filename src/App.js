26827603a4c29d2945aca1d34b086f18a5063bc9
Magali Sol <magalisol2307@gmail.com>
2022-09-14 21:58:00 -0300

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import  Router  from './app/Router';
 import 'materialize-css/dist/css/materialize.min.css';
 import './index.css'
 
-const App = ()=> {
+const App = ()=> {y
   return (
     <div className="App">
       <Router/> 
